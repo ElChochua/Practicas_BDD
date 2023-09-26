3d942ce3d46cee2298574110f42402433c2107b3
chochua <jossue.avll@gmail.com>
2023-09-25 21:45:46 -0700

    first commit

---
+++ BDD_Cliente/BDD_Cliente/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+namespace Practica1_ClienteServidor_Sockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Aplicacion Cliente**");
+            while (true)
+            {
+                try
+                {
+                    TcpClient tcpclnt = new TcpClient();
+                    Console.WriteLine("Conectando...");
+                    tcpclnt.Connect("127.0.0.1", 8001);
+                    Console.WriteLine("Conectando al servidor");
+                    Console.WriteLine("Introduzca la cadena a ejecutar");
+                    String str = Console.ReadLine();
+                    Stream stm = tcpclnt.GetStream();
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    Console.WriteLine("Transmitiendo cadena...");
+                    stm.Write(ba, 0, ba.Length);
+
+                    byte[] bb = new byte[100];
+                    int k = stm.Read(bb, 0, 100);
+                    string acuse = "";
+                    for(int i=0;i<k ; i++)
+                    {
+                        acuse = acuse + Convert.ToChar(bb[i]);
+                        Console.WriteLine(acuse);
+                        tcpclnt.Close();
+                    }
+                }catch(Exception e)
+                {
+                    Console.WriteLine("Error..." + e.StackTrace);
+                }
+            }
+        }
+    }
+}
---
+++ BDD_Servidor/BDD_Servidor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Net;
+
+namespace Pracica1_ClienteServidor_Sockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Aplicacion Servidor**");
+            while (true)
+            {
+                try
+                {
+                    IPAddress ipAd = IPAddress.Any;
+
+                    TcpListener myList = new TcpListener(ipAd, 8001);
+                    myList.Start();
+                    Console.WriteLine("Servidor iniciado en el puerto 8001");
+                    Console.WriteLine("Local End Point: " + myList.LocalEndpoint);
+                    Console.WriteLine("Esperando Conexion...");
+                    Socket socket = myList.AcceptSocket();
+                    Console.WriteLine("Conexion recibida desde: " + socket.RemoteEndPoint);
+                    byte[] b = new byte[100];
+                    int k = socket.Receive(b);
+                    Console.WriteLine("Recibiendo...");
+
+                    string cadena = "";
+                    for(int i = 0; i < k; i++)
+                    {
+                        cadena = cadena + Convert.ToChar(b[i]);
+                    }
+                    Console.WriteLine(cadena);
+
+                    string connectSQL = "Server=localhost;database=BDD_Practica1;" + " Integrated Security=SSPI;";
+                    SqlConnection cm = new SqlConnection();
+                    cm.ConnectionString = connectSQL;
+                    cm.Open();
+
+                    SqlCommand cmd = new SqlCommand(cadena, cm);
+                    cmd.ExecuteNonQuery();
+                    cm.Close();
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    socket.Send(asen.GetBytes("Cadena recibida. comando ejecutado"));
+                    Console.WriteLine("\nConfirmacion Enviada");
+                    socket.Close();
+                    myList.Stop();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Error..." + e);
+                }
+            }
+        }
+    }
+}
